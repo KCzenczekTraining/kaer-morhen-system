23eabb72704d05abec7880afab37e3bc0ae562bb
Ciri <kinga.czenczek.trainings@gmail.comm>
2026-03-23 20:08:16 +0100

    phase 4: switching sqlite to postgresql

--- services/bestiary-service/bestiary_config/settings.py
+++ services/bestiary-service/bestiary_config/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
+import dj_database_url
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,11 +82,12 @@ WSGI_APPLICATION = 'bestiary_config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# bestiary_config/settings.py
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+    )
 }
 
 
